62302558fd38e8b424f600a51132a7a69995bc60
dieriba <dieriba.pro@gmail.com>
2022-12-31 23:27:31 +0100

    working on exec

--- built_in/export.c
+++ built_in/export.c
@@ -6,7 +6,7 @@
 /*   By: dtoure <dtoure@student42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 06:30:18 by dtoure            #+#    #+#             */
-/*   Updated: 2022/12/31 23:14:35 by dtoure           ###   ########.fr       */
+/*   Updated: 2022/12/31 23:26:50 by dtoure           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	export(t_data *data, t_node *prev_last, char *new_var)
 {
 	t_node	*new_last;
 
-	new_last = create_node(data, new_var, 1);
+	new_last = create_node(new_var, 't');
 	is_error(data, new_last, MALLOC_ERR);
 	prev_last -> next = new_last;
 	new_last -> prev = prev_last;
