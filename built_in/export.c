f3cc92690d9d6bda47d9b9920f6eaccd78c46616
Dieriba TOURE <dtoure@e2r4p23.clusters.42paris.fr>
2023-01-25 01:47:33 +0100

    shell

--- built_in/export.c
+++ built_in/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dtoure <dtoure@student42.fr>               +#+  +:+       +#+        */
+/*   By: dtoure <dtoure@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 06:30:18 by dtoure            #+#    #+#             */
-/*   Updated: 2022/12/31 23:26:50 by dtoure           ###   ########.fr       */
+/*   Updated: 2023/01/25 01:42:24 by dtoure           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@ void	export(t_data *data, t_node *prev_last, char *new_var)
 
 	new_last = create_node(new_var, 't');
 	is_error(data, new_last, MALLOC_ERR);
-	prev_last -> next = new_last;
-	new_last -> prev = prev_last;
-	data -> env -> last = new_last;
+	if (!data -> env -> start)
+	{
+		data -> env -> start = new_last;
+		data -> env -> last = new_last;
+	}
+	else
+	{
+		prev_last -> next = new_last;
+		new_last -> prev = prev_last;
+		data -> env -> last = new_last;
+	}
 }
